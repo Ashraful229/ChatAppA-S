5d634401de30845ade252ac888826de438cd812e
Ashraful229 <emonak52@gmail.com>
2022-03-12 21:05:45 +0600

    d

--- chatApp/src/main/java/repository/MassageRepository.java
+++
@@ -1,7 +0,0 @@
-package repository;
-
-import entity.Massage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MassageRepository extends JpaRepository<Massage, Long> {
-}
--- chatApp/src/main/java/repository/PeopleRepository.java
+++
@@ -1,7 +0,0 @@
-package repository;
-
-import entity.People;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PeopleRepository extends JpaRepository<People, Long> {
-}
